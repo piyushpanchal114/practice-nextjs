9c36ec70bd32370f9a509514e1b08e0d90c0cfd6
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-11-13 20:28:53 +0530

    Learned about Programatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const NewUser = () => {
-  return <div>NewUser</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <p>NewUser</p>
+      <button
+        onClick={() => router.push("/users")}
+        className="btn btn-secondary"
+      >
+        Create
+      </button>
+    </div>
+  );
 };
 
 export default NewUser;
