5885a02ba695eae89d206ee21014f149795f3825
piyush <piyushpanchal114@gmail.com>
2023-11-14 20:45:34 +0530

    Handling not found error with custom message

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <strong>400</strong> | The request page doesn't exists.
+    </div>
+  );
+};
+
+export default NotFoundPage;
