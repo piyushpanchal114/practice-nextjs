44eef1f28c565593dea19d2d0fe3571d82e32e35
piyush <piyushpanchal114@gmail.com>
2023-11-16 19:07:49 +0530

    Delete api via next

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -22,3 +22,11 @@ export async function PUT(
 
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10) return NextResponse.json({ error: "User not found." });
+  return NextResponse.json({});
+}
