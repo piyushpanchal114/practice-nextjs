2e6d83b91344b960e3810f0f376716713b4c3e56
piyush <piyushpanchal114@gmail.com>
2023-11-19 14:04:20 +0530

    create a update endpoint using prisma

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -16,17 +16,29 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found." }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: parseInt(params.id) },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser, { status: 200 });
 }
 
 export function DELETE(
